f8a32a93474c090337181e5ee2ebd34b5e530227
sravika2205 <S572588@nwmissouri.edu>
2024-01-24 17:22:09 -0600

    second git commit

---
+++ Helloworld/src/main/java/com/mycompany/helloworld/Example2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.helloworld;;
+
+/**
+ *
+ * @author S572588
+ */
+public class Example2 {
+    public static void main(String[]args){
+        System.out.println("Hello World. I am coming for you!");
+        
+    }
+}
